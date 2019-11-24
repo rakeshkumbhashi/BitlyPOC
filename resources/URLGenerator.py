57a0b40a4b8850ff99cb69b08f11fafdd8f1c7b6
Rakesh K Kumbhashi <rakesh.upadhyaya@gmail.com>
2019-11-24 17:17:46 +0530

    Added verifying GET inputs and URL not found

--- resources/URLGenerator.py
+++ resources/URLGenerator.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from flask import jsonify, request
+from flask_api import status
 from Models.Model import URLRep
 import sys
 import hashlib
@@ -8,10 +9,12 @@ import datetime
 class URLGen(Resource):
     def get(self):
         shorturlUser = request.args.get('shorturl')
-
-        #shorturlUser = query_parameters.get('shorturl')
+        if shorturlUser is None:
+            return "ShortURL input parameter missing", status.HTTP_400_BAD_REQUEST
 
         desired_documents = URLRep.objects(shorturl=shorturlUser)
+        if desired_documents.count() ==0:
+            return "No long URL found for input shorturl", status.HTTP_404_NOT_FOUND
         responseURLDoc = desired_documents[0]
 
         return jsonify(
--- resources/URLRedirect.py
+++ resources/URLRedirect.py
@@ -1,14 +1,20 @@
 from flask_restful import Resource
 from flask import jsonify, request, redirect
 from Models.Model import URLRep
-import sys
-import hashlib
-import datetime
+from flask_api import status
+
 
 class URLRedirect(Resource):
     def get(self):
         shorturlUser = request.args.get('shorturl')
+        if shorturlUser is None:
+            return "ShortURL input parameter missing", status.HTTP_400_BAD_REQUEST
+
         desired_documents = URLRep.objects(shorturl=shorturlUser)
+
+        if desired_documents.count() == 0:
+            return "No long URL found for input shorturl", status.HTTP_404_NOT_FOUND
+
         responseURLDoc = desired_documents[0]
 
         longurl = responseURLDoc["longurl"]
